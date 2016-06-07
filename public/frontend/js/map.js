3446d6620a706956cc9dc1ef13fbd14389404d7e
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-06-07 17:04:00 +0700

    update

--- public/frontend/js/map.js
+++ public/frontend/js/map.js
@@ -4,9 +4,12 @@ function initialize() {
     geocoder = new google.maps.Geocoder();
     var latlng = new google.maps.LatLng(27.1959739, 78.02423269999997);
     var mapOptions = {
-        zoom: 8,
+        zoom: 16, // initialize zoom level - the max value is 21
+        streetViewControl: false, // hide the yellow Street View pegman
+        scaleControl: true, // allow users to zoom the Google Map
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
         center: latlng
-    }
+    };
     map = new google.maps.Map(document.getElementById("googlemaps"), mapOptions);
 }
 
