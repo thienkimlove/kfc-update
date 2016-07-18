baa347a711a8bef1d53d394cc70e0a90a6dee1de
Đỗ Mạnh Quân <manhquan.do@ved.com.vn>
2016-07-18 10:26:32 +0700

    update

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -22,7 +22,7 @@
                     <input type="text" placeholder="" autocomplete="off">
                 </div>
                 <div class="box-map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.0521015084205!2d105.78402085075172!3d21.030601193021933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab4c76b12a3b%3A0x9a311c833456d5f0!2zRHV5IFTDom4sIEThu4tjaCBW4buNbmcsIEPhuqd1IEdp4bqleSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1463038745076" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1994.7632620687652!2d30.272845551218296!3d60.00241926382562!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4696343d7579cc91%3A0x935fad23a95a4186!2sBaykonurskaya+ul.%2C+14%2C+Sankt-Peterburg%2C+197227!5e0!3m2!1sen!2sru!4v1461628589853" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
                 </div>
                 <h3 class="title">
                     Career
